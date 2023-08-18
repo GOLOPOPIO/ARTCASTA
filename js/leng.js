16cd781696052776b213afb8ec0a0b12944b3f8c
GOLOPOPIO <dolgih11kirill@gmail.com>
2023-08-18 21:33:15 +0300

    leng changes

---
+++ js/leng.js
@@ -0,0 +1,56 @@
+let url_rus =
+  "http://127.0.0.1:5500/json/leng_rus.json";
+let url_eng =
+  "http://127.0.0.1:5500/json/leng_eng.json";
+
+let leng_rus = document.getElementById('leng_rus');
+let leng_rus_menu = document.getElementById('leng_rus_menu');
+let leng_eng = document.getElementById('leng_eng');
+let leng_eng_menu = document.getElementById('leng_eng_menu');
+
+let toggle_rus = function () {
+  leng_rus.classList.add('active');
+  leng_rus_menu.classList.add('active');
+  leng_eng.classList.remove('active');
+  leng_eng_menu.classList.remove('active');
+
+  updateLanguage(url_rus);
+}
+let toggle_eng = function () {
+  leng_rus.classList.remove('active');
+  leng_rus_menu.classList.remove('active');
+  leng_eng.classList.add('active');
+  leng_eng_menu.classList.add('active');
+
+  updateLanguage(url_eng);
+}
+
+let updateLanguage = function (url) {
+
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.responseType = "json";
+  request.send();
+
+  request.onload = function () {
+    let json = request.response;
+    console.log(json);
+
+    setLanguge(json);
+  }
+}
+
+function setLanguge(json) {
+  for (key in json) {
+    let el = document.getElementById(key);
+    el.textContent = '';
+    el.textContent = json[key];
+  }
+}
+
+updateLanguage(url_rus);
+
+leng_rus.addEventListener('click', toggle_rus);
+leng_rus_menu.addEventListener('click', toggle_rus);
+leng_eng.addEventListener('click', toggle_eng);
+leng_eng_menu.addEventListener('click', toggle_eng);
--- js/social_links.js
+++ js/social_links.js
@@ -1,5 +1,3 @@
-let vk = document.getElementById('vk');
-let tg = document.getElementById('tg');
-let wa = document.getElementById('wa');
+
 
 
